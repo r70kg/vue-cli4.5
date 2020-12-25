6c07b770c0dd60da069c1aa1d1130f9578b27a1c
zhangdawei <zhangdawei@cyberzone.cn>
2020-12-25 19:56:27 +0800

    axios封装 token失效 无痕刷新token处理

--- src/request/getrequest.js
+++ src/request/getrequest.js
@@ -5,7 +5,7 @@ import qs from "qs";
 /**
  * 请求封装为 myserver
  */
-function myserver(){
+function myserver() {
     this.server = server;
     this.nowhandle = null;
 }
@@ -18,7 +18,7 @@ function myserver(){
  */
 // !!! 利用全局混入Mixin绑定 Vue实例
 // 组件中调用    由this.msv.v(this)改为this.msv.模块名.接口名;
-myserver.prototype.v = function (ob){
+myserver.prototype.v = function (ob) {
     this.nowhandle = ob;
     return this;
 }
@@ -28,13 +28,13 @@ myserver.prototype.v = function (ob){
  * @param urlOb 模块内暴露的接口对象
  */
 
-myserver.prototype.parseRouter = function (name,urlOb){
-  var ob = this[name] = {};
-  Object.keys(urlOb).forEach((item)=>{
-     ob[item] = this.sendMes.bind(this,name,item,urlOb[item]);
-     //  赋值每个请求属性 state 
-     ob[item].state = 'ready';
-  })
+myserver.prototype.parseRouter = function (name, urlOb) {
+    var ob = this[name] = {};
+    Object.keys(urlOb).forEach((item) => {
+        ob[item] = this.sendMes.bind(this, name, item, urlOb[item]);
+        //  赋值每个请求属性 state
+        ob[item].state = 'ready';
+    })
 }
 
 /**
@@ -44,24 +44,26 @@ myserver.prototype.parseRouter = function (name,urlOb){
  * @param url     模块内接口对象的value字段 (接口)
  * @param config  用户传入的默认配置
  */
-myserver.prototype.sendMes = function (moduleName,name,url,config){
+myserver.prototype.sendMes = function (moduleName, name, url, config) {
     // 默认配置
-   var config = config || {};
-   var type = config.type || 'get';
-   var data = config.data || {};
-   var bindName = config.bindName || name;
+    var config = config || {};
+    var type = config.type || 'get';
+    var data = config.data || {};
+    var bindName = config.bindName || name;
 
-   var self = this;
+    var self = this;
     // 响应数据处理前==处理==响应数据处理后
-   var before = function(mes){
-       console.log(mes)
-       self[moduleName][name].state = 'ready';
-       // ！！return 后续才能获取到数据
-       return mes;
-   }
-   // 请求失败
-    var fail = function(err){
+    var before = function (mes) {
+        console.log(mes)
+        self[moduleName][name].state = 'ready';
+        // ！！return 后续才能获取到数据
+        return mes;
+    }
+    // 请求失败
+    var fail = function (err) {
         console.log(err)
+        self[moduleName][name].state = 'ready';
+        return err;
     }
 
     /**
@@ -71,33 +73,33 @@ myserver.prototype.sendMes = function (moduleName,name,url,config){
      * [通过myserver.prototype.v绑定this实现]
      *
      */
-   var defaultFn = function(mes){
+    var defaultFn = function (mes) {
         console.log(mes)
-        if(mes.data){
+        if (self&&self.nowhandle&&mes && mes.data) {
             self.nowhandle[bindName] = mes.data;
         }
-   }
-   //  传入数据处理函数，默认使用defaultFn 处理
-   var success=  config.success || defaultFn;
-   var callback = function(res){
-       success(res,defaultFn);
-   }
-
-   // 请求调用
-   var state = {
-       get:function(){
-           var urlqs = url + '?' + qs.stringify(data);
-           server.get(urlqs).then(before,fail).then(callback);
-       },
-       post:function(){
-        server.post(url,data).then(before,fail).then(callback);
     }
-   }
-   // 为每个请求绑定状态标识，防止阻碍并发请求 实现防止重复提交
-   if(self[moduleName][name].state == 'ready'){
-      self[moduleName][name].state = 'pending'
-      state[type]();
-   }
+    //  传入数据处理函数，默认使用defaultFn 处理
+    var success = config.success || defaultFn;
+    var callback = function (res) {
+        success(res, defaultFn);
+    }
+
+    // 请求调用
+    var state = {
+        get: function () {
+            var urlqs = url + '?' + qs.stringify(data);
+            server.get(urlqs).then(before, fail).then(callback);
+        },
+        post: function () {
+            server.post(url, data).then(before, fail).then(callback);
+        }
+    }
+    // 为每个请求绑定状态标识，防止阻碍并发请求 实现防止重复提交
+    if (self[moduleName][name].state == 'ready') {
+        self[moduleName][name].state = 'pending'
+        state[type]();
+    }
 }
 
 
--- src/request/server.js
+++ src/request/server.js
@@ -1,27 +1,29 @@
 /**
- * 请求时token失效时，重新拉取token;
+ * 请求时token失效，重新拉取token;
  * 同时把并发的失效请求储存,待新token拉取成功后 用newToken继续之前的请求
  */
 import axios from 'axios';
-import store from '../store';
+import store from '@/store';
 
 
 // 请求基本配置
 let config = {
-    baseUrl:process.env.VUE_APP_BASE_API,
-    timeout:5000
+    baseUrl: process.env.VUE_APP_BASE_API,
+    withCredentials: true, // send cookies when cross-domain requests
+    timeout: 5000
 }
 
-const server  = axios.create(config);
+const server = axios.create(config);
 
 
-const loginUrl = 'https://xxxxxxxx'  // 登录url
+const loginUrl = process.env.VUE_APP_LOGIN_URL  // 登录url
 let isRefreshing = false  // 是否正在刷新token
 let callbacks = [] // 失效后同时发送请求的容器 -- 缓存接口
 
 // 刷新 token 后, 将缓存的接口重新请求一次
 function onAccessTokenFetched(newToken) {
     callbacks.forEach(callback => {
+        console.log(callback)
         callback(newToken)
     })
     // 清空缓存接口
@@ -41,106 +43,82 @@ function addCallbacks(callback) {
 // request interceptor
 server.interceptors.request.use(
     config => {
-        /*
-           do something before request is sent
-           let each request carry token
-           ['X-Token'] is a custom headers key
-           please modify it according to the actual situation
-        */
-        //  根据项目需求确定参数
-        config.headers = {
-            accessToken:  store.state.user.token || '',
-            imei: '',
-            project: ''
-        }
-        if (store.getters.token) {
 
-            config.headers['X-Token'] = getToken()
-        }
+        //  根据项目需求确定参数
+        config.headers['Authorization'] = 'Bearer ' + store.state.user.access_token
         return config
     },
     error => {
-        // do something with request error
-        console.log(error) // for debug
-        // return Promise.reject(error)
+
+        console.log(error)
+        return Promise.reject(error)
     }
 )
 
 // response interceptor
 server.interceptors.response.use(
     response => {
-        const res = response.data
-        console.log(res)
 
-        if (res.statusCode === 200) {
+        const res = response.data || ''
+        const options = response.config
+
+        // 当前请求url是否为登陆接口
+        const _isLoginUrl = options.url.includes(loginUrl);
+
+        // 后端返回的成功状态码
+        if (res.code === 200) {
             return res
-        } else if (res.statusCode === 401 && !options.url.includes(loginUrl)) { // 如果当前状态码为401并且请求的不是登陆接口
+
+            // 如果当前状态码为401并且请求的不是登陆接口
+        } else if (res.code === 401 && !_isLoginUrl) {
 
             /**
-             * 将未授权接口缓存起来 retryOriginalRequest 这个 Promise 函数很关键，它一直处于等待状态。
-             * 只有当token刷新成功后，onAccessTokenFetched 这个函数执行了回调函数，返回了 resolve 状态
+             * 将401状态接口缓存 retryOriginalRequest
+             * 用 Promise 使其处于等待状态
+             * 当token刷新成功，onAccessTokenFetched 这个函数执行了回调函数，返回 resolve 状态
              * @type {Promise<any>}
              */
 
             const retryOriginalRequest = new Promise(resolve => {
                 addCallbacks(newToken => {
+
                     // 表示用新的token去替换掉原来的token
-                    options.header.Authorization = newToken
+                    options.headers.Authorization = newToken
                     // 替换掉url -- 因为baseURL会扩展请求url
                     options.url = options.url.replace(process.env.VUE_APP_BASE_API, '')
-                    resolve(service.request(options)) // 调用resolve请求队列里面接口
+                    // 调用resolve请求队列里面接口
+                    resolve(server.request(options))
                 })
 
                 // 无感刷新Token
                 if (!isRefreshing) {
                     isRefreshing = true
-                    refreshToken().then(res => {  // 用rftoken获取新的token
-                        const newToken = res.token
-                        const newRefreshToken = res.refreshToken
-                        // setToken()
-                        // setRefreshToken()
+                    store.dispatch('refreshToken').then((res2) => {
+                        const newToken = store.state.user.access_token;
                         onAccessTokenFetched(newToken)
-                    }).catch(() => {
-                        // 刷新token报错的话, 就需要跳转到登录页面
-                        // setToken('')
-                        // setRefreshToken('')
 
-                        alert( res.message || 'Error')
-                        // to re-login
+                    }).catch((err) => {
+                        // 跳转到登录页面并清空 token
+                        alert(err.message || 'Error')
 
-                        // store.dispatch('user/resetToken').then(() => {
-                        //     location.reload()
-                        // })
+                        store.dispatch('resetToken').then(() => {
+                            location.reload()
+                        })
                     }).finally(() => {
                         isRefreshing = false
                     })
                 }
             })
-            return retryOriginalRequest // 将token过期期间请求的接口包装成promise返回，等待刷新token后重新请求
+            // 将token过期期间请求的接口包装成promise返回，等待刷新token后重新请求
+            return retryOriginalRequest
         } else {
             return res || 'Error'
-            // return Promise.reject(new Error(res || 'Error'))
         }
     },
     error => {
-        console.log('err' + error) // for debug
         return Promise.reject(error)
     }
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default server;
